44c34b3b84a171757cefc715059b07bae0e39c13
Irelandoracle1 <nairajnr1@gmail.com>
2024-10-23 15:20:32 +0000

    add code to increment score and incorrect answer score

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,9 +36,11 @@ function checkAnswer(){
 
     if(isCorrect){
         alert("Hey you got it right! :D")
+        incrementScore();
     }
     else{
         alert(`Aww............ you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`)
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -59,11 +61,14 @@ function calculateCorrectAnswer(){
 }
 
 function incrementScore(){
+    let oldScore = parseInt(document.getElementById("score").innerText)
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText)
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
